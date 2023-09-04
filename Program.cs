2e24e878d64c6a837cc3f94af40ed5cb59d06dbc
JeanCRECHRIOU <jchev34@gmail.com>
2023-09-04 13:33:42 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.Extensions.DependencyInjection;
+
+
+var collection = new ServiceCollection();
+collection.AddSingleton<Singleton>();
+collection.AddScoped<Scope>();
+collection.AddTransient<Transient>();
+
+var builder = collection.BuildServiceProvider();
+
+Parallel.For(1, 10, i =>
+{
+    //Console.Clear();
+    Console.WriteLine($"SingletonId = {builder.GetService<Singleton>().GetHashCode().ToString()}");
+    Console.WriteLine($"TransientId = {builder.GetService<Transient>().GetHashCode().ToString()}");
+    Console.WriteLine($"ScopeId = {builder.GetService<Scope>().GetHashCode().ToString()}");
+});
+
+
+
+public class Scope { }
+public class Transient{}
+public class Singleton { }
